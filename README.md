4741bf9b0257fabb3d26c68b011664bd9fd47471
adventure-star <Boristolstyakov@protonmail.com>
2021-01-20 09:30:11 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Laravel 8 - Vue 3 - TailwindCSS 2
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+Full setup and blank boilerplate for you and me :)
+
+Clone it, run "npm i" and "composer install", rename .env.example in .env and run "php artisan key:generate" thats it.
+
+## Author
+
+👤 **Miroljub Grujin <miro.grujin@hotmail.com>**
+
+- [GitHub](https://github.com/mcmiro)
+
+## License
+
+This project is open source and available under the [MIT License](LICENSE).
---
+++ resources/js/components/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container mx-auto mt-32">
+    <section>
+      <div class="px-4 sm:px-6 md:px-8 mb-10 sm:mb-16 md:mb-20 text-center">
+        <h2
+          class="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase mb-3"
+        >
+          Boilerplate
+        </h2>
+        <p
+          class="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-8"
+        >
+          Laravel 8 - Vue 3 - TailwindCSS 2
+        </p>
+        <p
+          class="max-w-4xl text-base sm:text-xl font-medium sm:leading-10 space-y-6 mb-6"
+        >
+          Full setup
+          <span class="bg-gray-200 rounded-full px-4 py-1 mx-2"
+            >Laravel Framework 8.17.0</span
+          >
+          <span class="bg-gray-200 rounded-full px-4 py-1 mx-2">Vue 3.0.3</span>
+          <span class="bg-gray-200 rounded-full px-4 py-1 mx-2"
+            >TailwindCSS 2.0.1</span
+          >
+
+          blank boilerplate for developers, that will save your time :)
+        </p>
+        <p
+          class="max-w-4xl text-lg sm:text-2xl font-medium sm:leading-10 space-y-6 mb-6"
+        >
+          Have fun!
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Laravel, Vue, TailwindCSS");
+  },
+};
+</script>
